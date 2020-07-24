33e64f2016540741baca67a67a01a23febca2da2
MasatoMakino <unagiinu.g@gmail.com>
2020-07-24 17:27:21 +0900

    change : demo script.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 "use strict";
-exports.server = require("./bin").get("./dist", { ignore: "**/*.json" });
+exports.server = require("./").get("./dist", { ignore: "**/*.json" });
 
 exports.server_without_php = require("./bin").get("./dist", {
   ignore: "**/*.json",
