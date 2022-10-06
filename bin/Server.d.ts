d0b8ac7d59519f4b286eccff3a332ef2b1e8622b
MasatoMakino <makino.masato.g@gmail.com>
2022-10-06 13:38:36 +0900

    change : add ignore "bin"

--- bin/Server.d.ts
+++
@@ -1,8 +0,0 @@
-import { ServerGenerationOption } from "./ServerOption";
-/**
- * optionに従い、PHPサーバーかbrowserSyncのいずれかを起動する。
- * @param browserSync
- * @param option
- */
-export declare function startServer(browserSync: any, option: ServerGenerationOption): Promise<void>;
-//# sourceMappingURL=Server.d.ts.map
--- bin/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { InitOption } from "./ServerOption";
-/**
- * サーバー開始およびリロードタスクを取得する。
- * @param base webサーバーのルートになるディレクトリ
- * @param option
- */
-export declare function generateTask(base: string, option?: InitOption): Function;
-//# sourceMappingURL=index.d.ts.map
