bd7d55e7e82e28bd12512b5539c37599e4a34a61
Masato Makino <makino.masato.g@gmail.com>
2024-10-07 12:37:33 +0900

    Add test commands and update build workflow (#56)
    * add : test command
    
    * add : test for help command
    
    * add : test for base option
    
    * change : build workflow
    
    * add : timeout of test

---
+++ test/cli.test.js
@@ -0,0 +1,56 @@
+const test = require("node:test");
+const assert = require("assert");
+const { spawn, exec } = require("node:child_process");
+
+test("CLI help command should execute successfully", async () => {
+  await new Promise((resolve, reject) => {
+    exec("node bin/CLI.js --help", (error, stdout, stderr) => {
+      try {
+        assert.strictEqual(error, null);
+        assert.strictEqual(stderr, "");
+        assert(stdout.includes("Usage:"));
+        resolve();
+      } catch (err) {
+        reject(err);
+      }
+    });
+  });
+});
+
+test("gulptask-dev-server command should start server and wait", async () => {
+  await new Promise((resolve, reject) => {
+    const process = spawn("node", ["bin/CLI.js", "--base", "sample"]);
+
+    let stdout = "";
+    let stderr = "";
+
+    process.stdout.on("data", (data) => {
+      stdout += data.toString();
+      if (stdout.includes("Serving files from:")) {
+        try {
+          assert.strictEqual(stderr, "");
+          assert(stdout.includes("Serving files from:"));
+          process.kill(); // プロセスを終了
+          clearTimeout(timeioutID); // タイムアウトをクリア
+          resolve();
+        } catch (err) {
+          process.kill();
+          clearTimeout(timeioutID); // タイムアウトをクリア
+          reject(err);
+        }
+      }
+    });
+
+    process.stderr.on("data", (data) => {
+      stderr += data.toString();
+    });
+
+    // タイムアウトを設定して、サーバーが起動するのを待つ
+    const timeioutID = setTimeout(() => {
+      if (!stdout.includes("Serving files from:")) {
+        process.kill();
+        reject(new Error("Server did not start in time"));
+      }
+    }, 10000); // タイムアウトを10秒に延長
+  });
+});
